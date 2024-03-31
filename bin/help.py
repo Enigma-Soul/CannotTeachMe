defe38e97c0cc2cfb56f2f35fcb5a94e815b9bdd
Enigma_Soul <119654171+rongjianuo@users.noreply.github.com>
2024-03-31 19:09:12 +0800

    提交

---
+++ bin/help.py
@@ -0,0 +1,8 @@
+def help(cmd):
+    if cmd == 'help':
+        print("help [命令]")
+    else:
+        print("help 帮助")
+        print("start 开始反控制")
+        print("stop 停止反控制")
+        print("renew 更新黑名单")
